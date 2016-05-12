5f2e063a13f07f3bc6089e153c1b50871e7d7af6
Amareshwari Sriramadasu <amareshwari@apache.org>
2016-05-12 14:54:09 +0530

    Fixed review comments

--- lens-server/src/main/java/org/apache/lens/server/error/GenericExceptionMapper.java
+++ lens-server/src/main/java/org/apache/lens/server/error/GenericExceptionMapper.java
@@ -21,7 +21,6 @@ package org.apache.lens.server.error;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
@@ -34,13 +33,15 @@ import org.apache.lens.server.model.LogSegregationContext;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
 
+import org.glassfish.jersey.server.ExtendedUriInfo;
+
 @Provider
 public class GenericExceptionMapper implements ExceptionMapper<Exception> {
   private final LogSegregationContext logContext;
   private final ErrorCollection errorCollection;
 
   @Context
-  UriInfo uriInfo;
+  ExtendedUriInfo extendedUriInfo;
 
   public GenericExceptionMapper() {
     logContext = LensServices.get().getLogSegregationContext();
@@ -49,43 +50,43 @@ public class GenericExceptionMapper implements ExceptionMapper<Exception> {
 
   @Override
   public Response toResponse(Exception exception) {
-    LensErrorTO errorTO;
-    Response.Status status;
-    if (exception instanceof WebApplicationException) {
+    Response.StatusType status;
+    String requestId = logContext.getLogSegragationId();
+    LensException le = null;
+
+    // Get http status code for the exception
+    if (exception instanceof LensException) {
+      le = ((LensException) exception);
+      le.buildLensErrorResponse(errorCollection, null, requestId);
+      status = le.getLensAPIResult().getHttpStatusCode();
+    } else if (exception instanceof WebApplicationException) {
       status = Response.Status.fromStatusCode(((WebApplicationException) exception).getResponse().getStatus());
     } else {
       status = Response.Status.INTERNAL_SERVER_ERROR;
     }
-    String requestId = logContext.getLogSegragationId();
-    // as only /queries and /preparedqueries are returning LensAPIResult this explicit check is done
-    // once other api moves to return LensAPIResult, check needs to be updated accordingly
-    // All api may not move to LensAPIResult, for example httpresultset result cannot be wrapped in LensAPIResult.
-    if (uriInfo.getPath().endsWith("/queries") || uriInfo.getPath().endsWith("/preparedqueries")) {
-      if (exception instanceof LensException) {
-        LensException e = ((LensException)exception);
-        e.buildLensErrorResponse(errorCollection, null, requestId);
 
-        final LensAPIResult lensAPIResult = ((LensException)exception).getLensAPIResult();
-        return Response.status(lensAPIResult.getHttpStatusCode()).entity(lensAPIResult).build();
+    if (extendedUriInfo.getMatchedResourceMethod().getInvocable().getRawResponseType() == LensAPIResult.class) {
+      if (le != null) {
+        return Response.status(status).entity(le.getLensAPIResult()).build();
       }
-      if (exception instanceof WebApplicationException) {
-        errorTO = LensErrorTO.composedOf(status.getStatusCode(), exception.getMessage(),
-          ExceptionUtils.getStackTrace(exception));
-      } else {
-        errorTO = LensErrorTO.composedOf(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),
-          "Internal server error" + (exception.getMessage() != null ? ":" + exception.getMessage() : ""),
-          ExceptionUtils.getStackTrace(exception));
-      }
-      LensAPIResult lensAPIResult = LensAPIResult.composedOf(null, logContext.getLogSegragationId(), errorTO,
-        status);
+      // if no LensException construct LensAPIResult
+      LensAPIResult lensAPIResult = constructLensAPIResult(exception, status);
       return Response.status(lensAPIResult.getHttpStatusCode()).entity(lensAPIResult).build();
     } else {
-      if (exception instanceof LensException) {
-        LensException e = ((LensException)exception);
-        e.buildLensErrorResponse(errorCollection, null, requestId);
-        return Response.status(e.getLensAPIResult().getHttpStatusCode()).entity(exception.getMessage()).build();
-      }
       return Response.status(status).entity(exception.getMessage()).build();
     }
+  }
+
+  private LensAPIResult constructLensAPIResult(Exception exception, Response.StatusType status) {
+    LensErrorTO errorTO;
+    if (exception instanceof WebApplicationException) {
+      errorTO = LensErrorTO.composedOf(status.getStatusCode(), exception.getMessage(),
+        ExceptionUtils.getStackTrace(exception));
+    } else {
+      errorTO = LensErrorTO.composedOf(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+        "Internal server error" + (exception.getMessage() != null ? ":" + exception.getMessage() : ""),
+        ExceptionUtils.getStackTrace(exception));
+    }
+    return LensAPIResult.composedOf(null, logContext.getLogSegragationId(), errorTO, status);
   }
 }
